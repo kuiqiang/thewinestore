f1014695f5c5ed95b0f53874ea5a68aaab8bdfa3
Cheng <Cheng@ASUS>
2014-09-29 04:07:56 +0800

    Responsive layout completed

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,15 @@
+'use strict';
 
+(function () {
+  var wineStoreApp = angular.module('wineStoreApp', []);
+
+  wineStoreApp.controller('SearchFormController', function () {
+    this.formIsShown = false;
+
+    this.toggleForm = function () {
+      this.formIsShown = !this.formIsShown;
+    };
+
+    this.templates = [{name: 'searchForm', url: 'templates/search-form.html'}];
+  });
+})();
