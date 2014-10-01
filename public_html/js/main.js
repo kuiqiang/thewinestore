ebecca280318ddbe166839cbb7d17f54022bd21c
Cheng Kui Qiang <ck.93gm@gmail.com>
2014-10-02 00:23:21 +0800

    Responsive layout revised and finalized

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,15 +1,33 @@
 'use strict';
 
 (function () {
-  var wineStoreApp = angular.module('wineStoreApp', []);
+  var wineStoreApp = angular.module('wineStoreApp', ['ngAnimate']);
 
-  wineStoreApp.controller('SearchFormController', function () {
-    this.formIsShown = false;
+  wineStoreApp.controller('AppController', function ($scope) {
+    $scope.searchFormIsShown = false;
 
-    this.toggleForm = function () {
-      this.formIsShown = !this.formIsShown;
+    $scope.toggleSearchForm = function () {
+      $scope.searchFormIsShown = !$scope.searchFormIsShown;
+      if($scope.searchFormIsShown) {
+        $("#search-form").slideDown('600');
+      }
+      else {
+        $("#search-form").slideUp('600');
+      }
     };
 
-    this.templates = [{name: 'searchForm', url: 'templates/search-form.html'}];
+    $scope.templates = {};
+
+    $scope.search = {
+      wineName: "",
+      region: "",
+      wineryName: "",
+      minYears: "",
+      maxYears: "",
+      minCost: "",
+      maxCost: "",
+      minInStock: "",
+      minPurchases: ""
+    };
   });
 })();
